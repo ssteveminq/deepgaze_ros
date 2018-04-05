93910cae0d5c2bf408b0ba16b8626eb53dcb7896
ssteveminq <steveminq@gmail.com>
2018-04-04 20:59:26 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+
+### Instruction for Face_recognition package 
+
+Tested in ubuntu 16.04
+
+## 1. install dlib 
+  
+  ````
+$ cd ~/
+$ git clone https://github.com/davisking/dlib.git
+$ cd dlib
+$ mkdir build; cd build; cmake .. -DDLIB_USE_CUDA=0; cmake --build .
+$ cd ..
+$ sudo python setup.py install --no USE_AVX_INSTRUCTIONS --no DLIB_USE_CUDA
+````
+  
+
+## 2. install pip (if you use python 3.3 > pip3)
+  Cammand for installing pip 
+  
+  ```
+  $ sudo apt-get install python-pip
+  ```
+    
+  Check pip version
+ 
+  ```
+$ pip -V
+  ```
+
+## 3. face_recognition package
+  
+  ````
+$ sudo pip install --upgrade scipy
+$ sudo pip install face_recognition
+$ sudo pip install git+https://github.com/ageitgey/face_recognition_models
+  ````
+  
+## 4. install tensor_flow (pip install)
+  
+````
+$ pip install tensorflow
+````
+
+## 5. Run examples.
+
+````
+$ python test.py known_faces/ new_faces/
+````
+
+## 6. Run with ROS
+````
+$ rosrun face_recognition_ros test_ros
+````
+
+## 7. ROS service and action are available
+- face_service.py
+- face_action_server.py
+- take_face_photo_server.py
+- take_photo_server.py
+
+# Author
+Minkyu Kim (steveminq@utexas.edu)
+
+## Reference
+  
+ https://github.com/hcr-hsrproject/villa_perception/tree/master/face_recognition
+  
---
+++ scripts/ex_cnn_head_pose_estimation_images/head_pose_image.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+#Copyright (c) 2016 Massimiliano Patacchiola
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+#MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+#CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+#SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+import sys
+import roslib
+import rospy
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+import select, termios, tty
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+from os import listdir
+import os
+import tensorflow as tf
+import cv2
+import numpy as np
+from deepgaze.head_pose_estimation import CnnHeadPoseEstimator
+
+class Head_Dectectors(object):
+    def __init__(self, wait=0.0):
+        sess = tf.Session() #Launch the graph in a session.
+        self.my_head_pose_estimator = CnnHeadPoseEstimator(sess) #Head pose estimation object
+        self.roll_config_filepath = "/home/mk/hsr/attention_ws/src/interaction_functions/deepgaze_ros/etc/tensorflow/head_pose/roll/cnn_cccdd_30k.tf"
+        self.pitch_config_filepath = "/home/mk/hsr/attention_ws/src/interaction_functions/deepgaze_ros/etc/tensorflow/head_pose/pitch/cnn_cccdd_30k.tf"
+        self.yaw_config_filepath = "/home/mk/hsr/attention_ws/src/interaction_functions/deepgaze_ros/etc/tensorflow/head_pose/yaw/cnn_cccdd_30k"
+        # Load the weights from the configuration folders
+        self.my_head_pose_estimator.load_roll_variables(self.roll_config_filepath) 
+        self.my_head_pose_estimator.load_pitch_variables(self.pitch_config_filepath) 
+        self.my_head_pose_estimator.load_yaw_variables(self.yaw_config_filepath) 
+
+        image_topic = "/hsrb/head_rgbd_sensor/rgb/image_raw"
+	rospy.Subscriber(image_topic, Image, self.image_callback)
+        self.bridge = CvBridge()
+        # self.cv2_img = cv2.imread()
+        # self.image = cv2.imread(file_name)
+	
+    def test_local_images(self): 
+        for i in range(1,9):
+            file_name = str(i) + ".jpg"
+            print("Processing image ..... " + file_name)
+            image = cv2.imread(file_name) #Read the image with OpenCV
+           # Get the angles for roll, pitch and yaw
+            roll = self.my_head_pose_estimator.return_roll(image)  # Evaluate the roll angle using a CNN
+            pitch = self.my_head_pose_estimator.return_pitch(image)  # Evaluate the pitch angle using a CNN
+            yaw = self.my_head_pose_estimator.return_yaw(image)  # Evaluate the yaw angle using a CNN
+            print("Estimated [roll, pitch, yaw] ..... [" + str(roll[0,0,0]) + "," + str(pitch[0,0,0]) + "," + str(yaw[0,0,0])  + "]")
+            print("")
+
+    def image_callback(self,msg):
+        print('image_callback: ')
+        # rospy.loginfo("image is of type: "+str(type(msg)))
+        self.cv2_img   = self.bridge.imgmsg_to_cv2(msg,"bgr8")
+        height, width, channels = self.cv2_img.shape
+        print height, width, channels 
+        cv2.imshow("image_window", self.cv2_img)
+
+        # roll = self.my_head_pose_estimator.return_roll(self.cv2_img)  # Evaluate the roll angle using a CNN
+        # pitch = self.my_head_pose_estimator.return_pitch(cv2_img)  # Evaluate the pitch angle using a CNN
+        # yaw = self.my_head_pose_estimator.return_yaw(cv2_img)  # Evaluate the yaw angle using a CNN
+        # print("Estimated [roll, pitch, yaw] ..... [" + str(roll[0,0,0]) + "," + str(pitch[0,0,0]) + "," + str(yaw[0,0,0])  + "]")
+
+    def listener(self,wait=0.0):
+        rospy.spin() 
+
+if __name__ == '__main__':
+        rospy.init_node('head_orientation_estimation_test')
+	# print("Initialize node")
+	Head_manager = Head_Dectectors()
+	Head_manager.listener()	
+
