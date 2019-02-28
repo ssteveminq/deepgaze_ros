13579808ee61a50030b39e7bcf69cae2cdc33e8f
ssteveminq <steveminq@gmail.com>
2019-02-28 15:46:48 -0600

    multi_track_action_server added

---
+++ scripts/object_tracking/multitrack_client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import hsrb_interface
+from hsrb_interface import geometry
+import rospy
+import sys
+import tf
+import actionlib
+import geometry_msgs.msg
+from deepgaze_ros.msg import *
+
+
+def mains():
+    # Initialize
+    client = actionlib.SimpleActionClient('multi_tracker_action',deepgaze_ros.msg.MultiTrackAction)
+    client.wait_for_server()
+    goal = deepgaze_ros.msg.MultiTrackGoal()
+    goal.idset.data.append(0)
+    goal.idset.data.append(1)
+    client.send_goal(goal)
+    client.wait_for_result()
+    rospy.loginfo("start action")
+        
+if __name__ == '__main__':
+    rospy.init_node('multi_track_client')
+    mains()
